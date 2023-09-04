2dc780aa706cbcc27dc7e1fbb8ca2ff6e7e36d35
Rebun13 <rubengdopazo@gmail.com>
2023-09-04 12:50:51 +0100

    Corrected the code

--- TheBombermanGame.cpp
+++ TheBombermanGame.cpp
@@ -16,30 +16,77 @@ vector<string> split(const string &);
  *  2. STRING_ARRAY grid
  */
 
-vector<string> bomberMan(int n, vector<string> grid) {
-    int turn = 0;
+void printGrid(vector<string> grid) {
+    cout << "______________________" << endl;
+    for(string s : grid) cout << s << endl;
+    cout << "______________________" << endl;
+}
 
+vector<string> bomberManAlt(int n, vector<string> grid) {
+    int turn = 0;
+    vector<string> postExplosionGrid;
     while(n > 0) {
-        if (turn == 2) {
+        if (turn == 1) {
+            postExplosionGrid = grid;
+            grid = vector<string> (grid.size(), string(grid[0].size(), 'O'));
+        } else if (turn == 2) {
             // Bombs explode
             turn++;
-            vector<string> newGrid(grid.size(), string(grid[0].size(), 'O'));
-            for(int i = 0; i < grid.size(); i++) {
-                for(int j = 0; j < grid[i].size(); j++) {
-                    if(grid[i][j] == 'O') { // There is a bomb here!
-                        if(i > 0) newGrid[i-1][j] = '.';
-                        if(j > 0) newGrid[i][j-1] = '.';
-                        newGrid[i][j] = '.';
-                        if(j < grid[i].size() - 1) newGrid[i][j+1] = '.';
-                        if(i < grid.size() - 1) newGrid[i+1][j] = '.';
+            for(int i = 0; i < postExplosionGrid.size(); i++) {
+                for(int j = 0; j < postExplosionGrid[i].size(); j++) {
+                    if(postExplosionGrid[i][j] == 'O') { // There is a bomb here!
+                        if(i > 0) grid[i-1][j] = '.';
+                        if(j > 0) grid[i][j-1] = '.';
+                        grid[i][j] = '.';
+                        if(j < postExplosionGrid[i].size() - 1) grid[i][j+1] = '.';
+                        if(i < postExplosionGrid.size() - 1) grid[i+1][j] = '.';
                     }
                 }
             }
-            grid = newGrid;
             turn = 0;
         }
+        printGrid(grid);
         turn++;
         n--;
+    }
+    return grid;
+}
+
+vector<string> complementaryGrid(vector<string> grid) {
+    int rows = grid.size();
+    int columns = grid[0].size();
+    vector<string> bombGrid(rows, string(columns, 'O'));
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++) {
+            if(grid[i][j] == 'O') { // There is a bomb here!
+                if(i > 0) bombGrid[i-1][j] = '.';
+                if(j > 0) bombGrid[i][j-1] = '.';
+                bombGrid[i][j] = '.';
+                if(j < columns - 1) bombGrid[i][j+1] = '.';
+                if(i < rows - 1) bombGrid[i+1][j] = '.';
+            }
+        }
+    }
+    return bombGrid;
+}
+
+vector<string> bomberMan(int n, vector<string> grid) {
+    // n -> 1 : res = grid
+    // n -> 2 : res = all bombs
+    // n -> 3 : res = complementary grid
+    // n -> 4 : res = all bombs
+    if(n == 1) return grid;
+
+    switch (n % 4) {
+    case 1:
+        return complementaryGrid(complementaryGrid(grid));
+        break;
+    case 3:
+        return complementaryGrid(grid);
+        break;
+    default:
+        return vector<string> (grid.size(), string(grid[0].size(), 'O'));
+        break;
     }
     return grid;
 }
